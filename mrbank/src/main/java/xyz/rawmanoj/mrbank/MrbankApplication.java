0975983344227bafcaa737f92d36fb49da41cce8
PahadiBlood <manojrawat612@gmail.com>
2026-09-20 17:50:48 +0530

    refactor: rename MrbankApplication to MrBankApplication

--- mrbank/src/main/java/xyz/rawmanoj/mrbank/MrbankApplication.java
+++ mrbank/src/main/java/xyz/rawmanoj/mrbank/MrbankApplication.java
@@ -2,12 +2,14 @@ package xyz.rawmanoj.mrbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
-public class MrbankApplication {
+public class MrBankApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MrbankApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MrBankApplication.class, args);
+    }
 
 }
